6a49c0e1d1f44ae2d4cde8e864ca9fc1c860343f
BlackSamsung <siddharthshringi@gmail.com>
2019-03-11 21:41:59 +0530

    write renderer class and use this into registeration api view

--- expenseTracker/serializers.py
+++ expenseTracker/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Income, Expense, Category
+from .models import Income, Expense, Category, User
 
 
 class IncomeSerializer(serializers.ModelSerializer):
@@ -21,17 +21,22 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 # create a endpoint for registering a user
-class RegistrationSerializer(serializers.ModelSerializer):
-    """Serializer for registering request and create new user"""
 
-    # Ensure passowrds are atleast 8 characters long, no longer than 128
-    # character and can not read by the client.
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """Serializers registration requests and creates a new user."""
+
+    # Ensure passwords are at least 8 characters long, no longer than 128
+    # characters, and can not be read by the client.
     password = serializers.CharField(
         max_length=128,
         min_length=8,
         write_only=True
     )
 
+    # The client should not be able to send a token along with a registration
+    # request. Making `token` read-only handles that for us.
     token = serializers.CharField(max_length=255, read_only=True)
 
     class Meta:
@@ -41,5 +46,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ['email', 'username', 'password', 'token']
 
     def create(self, validated_data):
-        # Use the create_user function to create a new user.
+        print(validated_data)
+        # Use the `create_user` method we wrote earlier to create a new user.
         return User.objects.create_user(**validated_data)
