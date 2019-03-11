4eb6c5028e1a1ea8b258ac47f886644503780dfe
BlackSamsung <siddharthshringi@gmail.com>
2019-03-11 13:42:41 +0530

    make registration serializer

--- expenseTracker/serializers.py
+++ expenseTracker/serializers.py
@@ -18,3 +18,28 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+# create a endpoint for registering a user
+class RegistrationSerializer(serializers.ModelSerializer):
+    """Serializer for registering request and create new user"""
+
+    # Ensure passowrds are atleast 8 characters long, no longer than 128
+    # character and can not read by the client.
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        # List all of the fields that could possibly be included in a request
+        # or response, including fields specified explicitly above.
+        fields = ['email', 'username', 'password', 'token']
+
+    def create(self, validated_data):
+        # Use the create_user function to create a new user.
+        return User.objects.create_user(**validated_data)
