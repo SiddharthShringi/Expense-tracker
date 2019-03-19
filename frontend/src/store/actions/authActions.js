c19b3868609b36cff3ebd59aeae966ffa33ffaf7
BlackSamsung <siddharthshringi@gmail.com>
2019-03-20 00:00:25 +0530

    write signIn action

--- frontend/src/store/actions/authActions.js
+++ frontend/src/store/actions/authActions.js
@@ -1,12 +1,47 @@
-export const signUp = newUser => {
+export const signUp = (newUser, cb) => {
   return dispatch => {
     fetch("http://127.0.0.1:8000/api/users/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({user:newUser})
-    }).then(res => res.json()).then(data => console.log(data) )
-  }
+      body: JSON.stringify({ user: newUser })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (!data.errors) {
+          const token = data.user.token;
+          const email = data.user.email;
+          const username = data.user.username;
+
+          localStorage.setItem("expense-jwt", `token ${token}`);
+          dispatch({
+            type: "SIGNUP_SUCCESS",
+            email,
+            username
+          });
+          cb(true);
+        } else {
+          const errors = data.errors
+          cb(false, errors)
+        }
+      });
+  };
 };
 
+export const singIn = user => {
+  return dispatch => {
+    fetch("http://127.0.0.1:8000/api/users/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({user})
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
+}
