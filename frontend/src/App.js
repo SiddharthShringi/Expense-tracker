5ba98a9c0f112bac99b7104701136511e4defd5c
BlackSamsung <siddharthshringi@gmail.com>
2019-03-20 00:00:58 +0530

    add signIn component to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import SignUp from '../src/components/auth/signUp'
+import SignUp from '../src/components/auth/SignUp'
+import SignIn from '../src/components/auth/SignIn'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <SignUp />
+      <SignIn />
     );
   }
 }
