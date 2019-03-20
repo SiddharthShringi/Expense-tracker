22fce6fdef0e4a127ab8e421fc0d72b9d0628512
BlackSamsung <siddharthshringi@gmail.com>
2019-03-20 05:54:46 +0530

    add react router to the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import SignUp from '../src/components/auth/SignUp'
 import SignIn from '../src/components/auth/SignIn'
+import Home from '../src/components/layout/Home'
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <SignIn />
+      <BrowserRouter>
+        <div className="app">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/signUp' component={SignUp} />
+            <Route path='/signIn' component={SignIn} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
