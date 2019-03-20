e6f891af0152c5263baabd2c12be10b89c2499c0
BlackSamsung <siddharthshringi@gmail.com>
2019-03-20 05:53:21 +0530

    write new signout action

--- frontend/src/store/actions/authActions.js
+++ frontend/src/store/actions/authActions.js
@@ -9,7 +9,6 @@ export const signUp = (newUser, cb) => {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data)
         if (!data.errors) {
           const token = data.user.token;
           const email = data.user.email;
@@ -30,7 +29,7 @@ export const signUp = (newUser, cb) => {
   };
 };
 
-export const singIn = user => {
+export const singIn = (user, cb) => {
   return dispatch => {
     fetch("http://127.0.0.1:8000/api/users/login/", {
       method: "POST",
@@ -41,7 +40,28 @@ export const singIn = user => {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data)
+        if(!data.errors) {
+          const token = data.user.token
+          const email = data.user.email
+
+          localStorage.setItem("expense-jwt", `token ${token}`);
+          dispatch({
+            type: "LOGIN_SUCCESS",
+            email
+          })
+          cb(true)
+        } else {
+          const errors = data.errors
+          cb(false, errors)
+        }
       })
+  }
+}
+
+export const signOut = (cb) => {
+  localStorage.clear()
+  cb()
+  return {
+    type: "SIGNOUT_USER"
   }
 }
