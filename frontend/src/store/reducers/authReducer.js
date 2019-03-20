5500721687fb45b5b2806388cfd8c1a3514ab9e9
BlackSamsung <siddharthshringi@gmail.com>
2019-03-20 05:58:27 +0530

    make case signOut for authReducer function

--- frontend/src/store/reducers/authReducer.js
+++ frontend/src/store/reducers/authReducer.js
@@ -1,15 +1,26 @@
 const initState = {
   username: null,
-	email: null,
+  email: null
 };
 
 const authReducer = (state = initState, action) => {
-	switch(action.type){
-		case "SIGNUP_SUCCESS":
-			return {...state, username:action.username, email:action.email}
-		default:
-		return state
-	}
+  switch (action.type) {
+    case "SIGNUP_SUCCESS":
+      return { ...state, username: action.username, email: action.email };
+
+		case "LOGIN_SUCCESS":
+		console.log("in red lin");
+		
+      return { ...state, email: action.email };
+
+		case "SIGNOUT_USER":
+			console.log("in red sout");
+			
+			return {...state, username:null, email:null}
+
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
